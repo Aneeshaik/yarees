4c50552181da76d1e42f3406b158c882f5e286fc
Aneeshaik <aneesnafeesimmu@gmail.com>
2025-05-16 11:19:55 +0530

    env-changes

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDcO94_zLWSmQd7rueOZCu401nz3-zTbAM",
-  authDomain: "yarees-05.firebaseapp.com",
-  projectId: "yarees-05",
-  storageBucket: "yarees-05.firebasestorage.app",
-  messagingSenderId: "319243664400",
-  appId: "1:319243664400:web:40b8091ec5fc2908029e95"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDERID,
+  appId: import.meta.env.VITE_FIREBASE_APPID
 };
 
 // Initialize Firebase
@@ -20,3 +20,5 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app)
+
+export default app
